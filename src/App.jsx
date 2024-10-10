40089164e34f52dcafcb33ac9d74d6e54e92c1a7
Shivam5672 <shivambansal.ind@gmail.com>
2024-10-10 19:05:19 +0530

    Version 1.01 Correct the path of App.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import 'src/App.css'
+import ''
 import Loader from './components/Loader';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
